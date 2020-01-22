3a444882872f35ab9c24b7c7f3f361a45bb09971
SoftCancer <ygxzzu@163.com>
2020-01-23 02:03:54 +0800

    实现注册中心EurekaServer 功能。

--- SpringCloudParent/cloud_eureka_01/src/main/java/com/yaogx/cloud_eureka_01/CloudEureka01Application.java
+++ SpringCloudParent/cloud_eureka_01/src/main/java/com/yaogx/cloud_eureka_01/CloudEureka01Application.java
@@ -2,8 +2,10 @@ package com.yaogx.cloud_eureka_01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer  // 开启EurekaServer 注册中心
 public class CloudEureka01Application {
 
     public static void main(String[] args) {
