03c17ee3897b636917325b6c33060c3ce2253971
SoftCancer <ygxzzu@163.com>
2020-01-22 20:11:13 +0800

    初次提交多模块 框架。包含父模块和子模块以及基础的配置，能成功启动。

---
+++ SpringCloudParent/cloud_eureka_01/src/main/java/com/yaogx/cloud_eureka_01/CloudEureka01Application.java
@@ -0,0 +1,13 @@
+package com.yaogx.cloud_eureka_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEureka01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEureka01Application.class, args);
+    }
+
+}
