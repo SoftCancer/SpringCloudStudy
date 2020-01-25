8da37c154c15b9a5b202317cf5e76d63234aba92
SoftCancer <ygxzzu@163.com>
2020-01-25 22:01:50 +0800

    实现hystrix 的监控

--- SpringCloudParent/cloud_eureka_hystrix_02/src/main/java/com/yaogx/cloud_eureka_hystrix_02/CloudHystrixApplication.java
+++ SpringCloudParent/cloud_eureka_hystrix_02/src/main/java/com/yaogx/cloud_eureka_hystrix_02/CloudHystrixApplication.java
@@ -2,7 +2,9 @@ package com.yaogx.cloud_eureka_hystrix_02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -13,6 +15,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients   //开启Feign的功能
+// 熔断器监控
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class CloudHystrixApplication {
 
     public static void main(String[] args) {
